9a00c09fdcb718c05bcc03224b9ef4e69c9ab90e
Tiago Donizetti Gomes <tiago.gomes@ifsp.edu.br>
2018-05-19 18:26:31 -0300

    Manchetes

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>
-        <?php wp_title(''); ?>
+        <?php wp_title(); ?>
     </title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
