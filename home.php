6aa7c5707198f855c10a01f2bf401de25f0d7b35
Tiago Donizetti Gomes <tiago.gomes@ifsp.edu.br>
2018-05-24 02:31:16 -0300

    Mensagens pós instalação

--- home.php
+++ home.php
@@ -3,10 +3,20 @@
     <?php include __DIR__ . '/page-mapa-do-site.php' ?>
 
 <?php else : ?>
-
-
-<?php get_header(); ?>
-	<?php dynamic_sidebar( 'miolo-central-1' ); ?>
-<?php get_footer(); ?>
+	<?php get_header(); ?>
+	<?php if (count( get_option('sidebars_widgets')['miolo-central-1'] ) == 0): ?>  
+		<div class="chapeu">Introdução</div> 
+		<h2>Página inicial</h2>
+		<?php if (current_user_can( 'administrator' ) ) : ?>        
+			<p>Esta área central não foi personalizada. 
+			Clique 
+			<a href="<?= site_url() ?>/wp-admin/customize.php?url=<?= htmlentities(site_url() . '/?' . $_SERVER["QUERY_STRING"])  ?>">aqui</a> 
+			para personalizar.</p>
+		<?php else: ?> 
+			<p>Bem-vindo ao nosso site. No momento ainda não há conteúdo.</p>
+		<?php endif; ?> 
+	<?php endif; ?> 
+	<?php dynamic_sidebar( 'miolo-central-1' ); ?>	
+	<?php get_footer(); ?>
 
 <?php endif; ?>
--- page-mapa-do-site.php
+++ page-mapa-do-site.php
@@ -2,18 +2,16 @@
 
 <h2>Mapa do site</h2>
 
-<?php if (count( get_option('sidebars_widgets')['mapa-do-site-1'] ) > 0): ?>
-
-    <?php dynamic_sidebar( 'mapa-do-site-1' ); ?>
-
-<?php else: ?> 
-    <?php if (current_user_can( 'administrator' ) ) : ?>
-        <p>Esta área não foi personalizada. Utilize widgets nesta página para inserir conteúdo.</p>
-        <?php dynamic_sidebar( 'mapa-do-site-1' ); ?>
+<?php if (count( get_option('sidebars_widgets')['mapa-do-site-1'] ) == 0): ?>   
+    <?php if (current_user_can( 'administrator' ) ) : ?>        
+        <p>Esta área não foi personalizada. 
+        Clique 
+        <a href="<?= site_url() ?>/wp-admin/customize.php?url=<?= htmlentities(site_url() . '/?' . $_SERVER["QUERY_STRING"])  ?>">aqui</a> 
+        para personalizar.</p>
     <?php else: ?> 
         <p>Sem conteúdo.</p>
     <?php endif; ?> 
-
-<?php endif; ?>   
+<?php endif; ?> 
+<?php dynamic_sidebar( 'mapa-do-site-1' ); ?>
 
 <?php get_footer(); ?>
